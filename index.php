f1a6973631314c342d7197784949eacdea00be5c
aepCyclonee <lyotosamb@gmail.com>
2026-08-13 11:16:42 -0300

    global

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php
-    echo "Hello World!";
-    $sim = "mambo";
-    $nao = 67;
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projeto Etec - DS</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello World!";
+            $sim = "mambo";
+            $nao = 67;
 
-    if ($sim = "mambo" && $nao = 67)
-        echo "Olá Mundo, mambo!";
-    else
-        echo "Olá Mundo, mas sem mambo";
-?>
+            if ($sim = "mambo" && $nao = 67)
+                echo "Olá Mundo, mambo!";
+            else
+                echo "Olá Mundo, mas sem mambo";
+        ?>
+    </body>
+</html>
