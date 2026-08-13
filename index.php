eaf95f4325e6980392ef32935297154debb5cd86
aepCyclonee <lyotosamb@gmail.com>
2026-08-13 10:56:23 -0300

    global

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
     echo "Hello World!";
     $sim = "mambo";
-    $nao = "67";
+    $nao = 67;
+
+    if ($sim = "mambo" && $nao = 67)
+        echo "Olá Mundo, mambo!";
+    else
+        echo "Olá Mundo, mas sem mambo";
 ?>
